59ab3dec1b9eaff75bd2ca1669c5793fa50d5eef
book1317 <bookboks@hotmail.com>
2020-09-22 18:04:59 +0700

    update database clound

--- database/connect.go
+++ database/connect.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,12 +13,21 @@ import (
 type Database struct{}
 
 func (Database) Connect() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://book1317:1317@cluster0.qifgh.gcp.mongodb.net/facebook?retryWrites=true&w=majority",
+	))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	err = client.Ping(context.TODO(), nil)
 
